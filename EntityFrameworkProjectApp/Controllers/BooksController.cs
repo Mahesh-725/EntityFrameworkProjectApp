eafa1e904219611d4488ca66ff126d8f08ba4290
Mahesh P <Mahesh.P@acginspectionsystems.onmicrosoft.com>
2025-09-27 15:01:41 +0530

    Insert/Add New Record in Entity Framework Core

---
+++ EntityFrameworkProjectApp/Controllers/BooksController.cs
@@ -0,0 +1,36 @@
+﻿using EntityFrameworkProjectApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkProjectApp.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public BooksController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> AddBooks([FromBody] Book model)
+        {
+            _appDbContext.Books.Add(model);
+            await _appDbContext.SaveChangesAsync();
+
+            return Ok(model);
+        }
+
+        [HttpGet("allBooks")]
+        public async Task<IActionResult> GetBooks()
+        {
+            var books=await _appDbContext.Books.ToListAsync();
+
+            return Ok(books);
+        }
+    }
+}
