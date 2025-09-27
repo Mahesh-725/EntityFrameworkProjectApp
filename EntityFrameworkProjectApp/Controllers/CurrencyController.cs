3cc1beb1a941ce0d30ba2a93770897f458a81fdc
Mahesh P <Mahesh.P@acginspectionsystems.onmicrosoft.com>
2025-09-27 11:43:25 +0530

    FirstAsync() vs FirstOrDefaultAsync() vs SingleAsync() vs SingleOrDefaultAsync() in EF Core

--- EntityFrameworkProjectApp/Controllers/CurrencyController.cs
+++ EntityFrameworkProjectApp/Controllers/CurrencyController.cs
@@ -25,12 +25,24 @@ namespace EntityFrameworkProjectApp.Controllers
             return Ok(currencies);
         }
 
-        [HttpGet("{id}")]  // retrieving the record using PK.
+        [HttpGet("{id:int}")]  // retrieving the record using PK.
         public async Task<IActionResult> GetCurrencyByIdAsync([FromRoute] int id)
         {
             var currencies = await _appDbContext.Currencies.FindAsync(id);
 
             return Ok(currencies);
         }
+
+        [HttpGet("{name}")]  // retrieving the record using PK.
+        public async Task<IActionResult> GetCurrencyByIdAsync([FromRoute] string name)
+        {
+            //var currencies = await _appDbContext.Currencies.Where(x => x.Title == name).FirstAsync();  //when data present retreives the data,and throws exception if no data match.
+            //var currencies = await _appDbContext.Currencies.Where(x => x.Title == name).SingleAsync(); //when data present retreives the data,and throws exception if no data match.
+            //var currencies = await _appDbContext.Currencies.Where(x => x.Title == name).SingleOrDefaultAsync(); // when data present retrieves the data and the data should unique if duplicates present throwa exception, give null if no data match.
+            var currencies = await _appDbContext.Currencies.Where(x => x.Title == name).FirstOrDefaultAsync();
+
+            return Ok(currencies);
+        }
     }
 }
+c
